54524664921be42a2cfe32890036e59a06dcdd20
shadowsight <30534190+shadowsight@users.noreply.github.com>
2021-01-07 10:07:08 -0600

    Catch up

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using WebA.Models;
+
+namespace WebA.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly ILogger<SongsController> _logger;
+
+
+
+        public SongsController(ILogger<SongsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Tools()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace WebA.Models
 {
     public class ErrorViewModel
